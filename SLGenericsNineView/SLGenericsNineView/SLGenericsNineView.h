4c241c5443fb88a576638a49d736cbf9ac8705b4
倪申雷 <610112090@qq.com>
2018-03-08 21:51:18 +0800

    项目初始化

---
+++ SLGenericsNineView/SLGenericsNineView/SLGenericsNineView.h
@@ -0,0 +1,19 @@
+//
+//  SLGenericsNineView.h
+//  SLGenericsNineView
+//
+//  Created by 倪申雷 on 2018/3/8.
+//  Copyright © 2018年 倪申雷. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SLGenericsNineView.
+FOUNDATION_EXPORT double SLGenericsNineViewVersionNumber;
+
+//! Project version string for SLGenericsNineView.
+FOUNDATION_EXPORT const unsigned char SLGenericsNineViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SLGenericsNineView/PublicHeader.h>
+
+
---
+++ SLGenericsNineView/SLGenericsNineView/Source/UIViewExt.swift
@@ -0,0 +1,19 @@
+//
+//  UIViewExt.swift
+//  SLUIKit
+//
+//  Created by 一声雷 on 2018/3/6.
+//  Copyright © 2018年 slni. All rights reserved.
+//
+
+import UIKit
+
+extension UIView{
+    // 移除所有的子控件
+    func removeAllSubviews(){
+        while (self.subviews.count > 0){
+            self.subviews.last?.removeFromSuperview()
+        }
+    }
+    
+}
